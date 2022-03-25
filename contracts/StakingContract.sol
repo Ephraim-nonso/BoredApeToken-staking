00a1ab39c817c3f2022c59bf57b8877d1380542d
Chukwu Ephraim <Chukwuchinonsoephraim@gmail.com>
2022-03-25 04:21:47 +0100

    Func changes

--- contracts/StakingContract.sol
+++ contracts/StakingContract.sol
@@ -25,24 +25,23 @@ contract StakingContract {
     event Deposit(address owner, uint256 time, uint256 _amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
 
-    constructor(address _token, address _apeNFT) {
+    constructor(address _token) {
         token = IERC20(_token);
-        apeNFT = IERC721(_apeNFT);
+        // apeNFT = IERC721(_apeNFT);
     }
 
     function stake(uint256 stake_) external returns (bool success) {
-        // Ensure that input equals the token decimals.
-        stake_ = stake_.mul((10**18));
-        require(apeNFT.balanceOf(msg.sender) >= 1, "No BoredApe NFT");
+        // require(apeNFT.balanceOf(msg.sender) >= 1, "No BoredApe NFT");
         require(token.balanceOf(msg.sender) >= stake_, "Insufficient funds");
         token.transferFrom(msg.sender, address(this), stake_);
-
+        uint256 deposit = reward(stake_);
         // Records the input into the struct.
         Staking storage s = stakings[msg.sender];
-        s.stake = stake_;
+        s.stake = s.stake + stake_;
         s.timeStart = block.timestamp;
-        s.timeDue = (block.timestamp).add(259200);
-        s.reward = reward(stake_);
+        // s.timeDue = (block.timestamp).add(259200);
+        s.timeDue = (block.timestamp).add(60);
+        s.reward = s.reward + deposit;
         s.balance = s.stake + s.reward;
 
         // Update state vaariables.
@@ -53,20 +52,23 @@ contract StakingContract {
         emit Deposit(msg.sender, block.timestamp, stake_);
     }
 
-    function reward(uint256 stake_) internal view returns (uint256 reward_) {
-        Staking memory s = stakings[msg.sender];
-        stake_ = stake_.mul((10**18));
+    function reward(uint256 stake_) public pure returns (uint256 reward_) {
+        require(stake_ > 0, "Invalid stake passed.");
         uint256 monthlyRate = ((stake_.mul(10)).div(100));
         uint256 dailyRate = monthlyRate.div(30);
-        require(stake_ > 0, "No record.");
-        require(stake_ > 0, "Invalid stake passed.");
+        return dailyRate;
+    }
+
+    function checkTimeForClaim() internal view returns (bool condition) {
+        Staking memory s = stakings[msg.sender];
         if ((block.timestamp >= s.timeDue) && (s.stake != 0)) {
-            if ((block.timestamp >= (s.timeDue + 86400)) && (s.stake != 0)) {
-                return dailyRate.mul(3) + dailyRate;
+            // if ((block.timestamp >= (s.timeDue + 86400)) && (s.balance != 0)) {
+            if ((block.timestamp >= (s.timeDue + 100)) && (s.balance != 0)) {
+                return true;
             }
-            return reward_ = dailyRate.mul(3);
+            return true;
         } else {
-            return 0;
+            return false;
         }
     }
 
@@ -75,8 +77,9 @@ contract StakingContract {
 
         // Ensure that amount to withdraw isn't more than rewards.
         require(_amount <= s.reward, "amount exceeds rewards.");
-        s.reward = s.reward.sub(_amount);
-        s.balance = s.balance.sub(_amount);
+        require(checkTimeForClaim(), "not up to valid date.");
+        s.reward = s.reward - _amount;
+        s.balance = s.balance - _amount;
         s.balance = s.stake;
         if (s.stake != 0) {
             s.reward = reward(s.stake);
@@ -84,14 +87,27 @@ contract StakingContract {
         }
         emit Withdraw(msg.sender, block.timestamp, s.reward);
         // Transfer from contract to staker.
-        success = token.transfer(msg.sender, s.reward);
+        success = token.transfer(msg.sender, _amount);
     }
 
     function withdrawAllInPool() external returns (bool) {
         Staking memory s = stakings[msg.sender];
-        require(block.timestamp > s.timeDue, "Not up to 3 days.");
-        // // Transfer from contract to staker.
+        // require(block.timestamp > s.timeDue, "Not up to 3 days.");
+        require(checkTimeForClaim(), "not up to valid date.");
+        token.transfer(msg.sender, s.balance);
+        s.success = false;
+        s.balance = 0;
+        s.stake = 0;
+        s.reward = 0;
+        s.timeStart = 0;
+        s.timeDue = 0;
+
+        // Transfer from contract to staker.
         emit Withdraw(msg.sender, block.timestamp, s.balance);
-        return token.transfer(msg.sender, s.balance);
+        return true;
+    }
+
+    function getRecord() external view returns (Staking memory c) {
+        c = stakings[msg.sender];
     }
 }
